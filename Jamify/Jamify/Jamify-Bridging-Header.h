81fa6e470686413e1ffda30b62fd395bf38f3b4c
WillieNilly <80725046+WillieNilly@users.noreply.github.com>
2021-07-12 20:31:01 -0400

    Add files via upload

---
+++ Jamify/Jamify/Jamify-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  Jamify-Bridging-Header.h
+//  Jamify
+//
+//  Created by Willie roberts on 7/12/21.
+//
+
+#ifndef Jamify_Bridging_Header_h
+#define Jamify_Bridging_Header_h
+
+#import <SpotifyiOS/SpotifyiOS.h>
+
+
+#endif /* Jamify_Bridging_Header_h */
---
+++ Jamify/Jamify/JamifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  JamifyApp.swift
+//  Jamify
+//
+//  Created by Willie roberts on 7/12/21.
+//
+
+import SwiftUI
+
+@main
+struct JamifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
